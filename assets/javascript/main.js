9d9ddabba4e73528b8d5402c9d4a1aa00ae1541f
Mike Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-02 14:46:40 -0400

    initial two column setup

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,23 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let lat;
     let lon;
 
-    navigator.geolocation.getCurrentPosition(function(response){
+    navigator.geolocation.getCurrentPosition(function (response) {
         lat = response.coords.latitude;
         lon = response.coords.longitude;
 
-        console.log(lat,lon);
+        console.log(`lat:${lat}, lon:${lon}`);
     })
+
+    $.ajax({
+        url: `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lon}`,
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
+        }
+    })
+        .then(function (response) {
+            console.log(response)
+        });
+
 })
 
-$.ajax({
-    url: "https://developers.zomato.com/api/v2.1/search?entity_id=7555&entity_type=city",
-    method: "GET",
-    headers:{
-        "Accept": "application/json",
-        "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
-}
-  })
-    .then(function(response) {
-        console.log(response)
-    });
