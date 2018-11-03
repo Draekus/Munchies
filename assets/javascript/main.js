a1e45fadfa709ce7f6918f0a3a67d8fa6509d74a
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-02 20:03:47 -0400

    added cards into main

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -18,8 +18,9 @@ $(document).ready(function () {
             "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
         }
     }).then(function (response) {
-        console.log(response);
-        restaurantList = response.restaurants.map(restaurant=>{console.log(restaurant.restaurant.name)});
+        //console.log(response);
+        response.restaurants.map(restaurant=>{console.log(restaurant.restaurant.name)});
+
     });
 
 })
