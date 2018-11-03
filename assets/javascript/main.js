08321375a280cae9dc0e27d5e57c116e0f5935ab
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-11-03 10:39:33 -0400

    slight changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,14 +7,16 @@ $(document).ready(function(){
          console.log(lat,lon);
     })
 })
- $.ajax({
-    url: "https://developers.zomato.com/api/v2.1/search?entity_id=7555&entity_type=city",
-    method: "GET",
-    headers:{
-        "Accept": "application/json",
-        "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
-}
-  })
-    .then(function(response) {
-        console.log(response)
-    });
+//  $.ajax({
+//     url: "https://developers.zomato.com/api/v2.1/search?entity_id=7555&entity_type=city",
+//     method: "GET",
+//     headers:{
+//         "Accept": "application/json",
+//         "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
+// }
+//   })
+//     .then(function(response) {
+//         console.log(response)
+//     });
+
+   
