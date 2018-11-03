bae20053fb5cd8d79ba90ccf1a34d6101382afc2
unknown <stanmozolevskiy@gmail.com>
2018-11-03 12:28:09 -0400

    google map with API key

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -33,6 +33,14 @@ $(document).ready(function () {
 
 });
 
-
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 43.080752, lng: -70.80219389999999};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
 
 
