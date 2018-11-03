7acd20d0cc6deef9ec9500f09e52a1b0ac60c495
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-03 12:36:00 -0400

    fixed location and getting data

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,7 +8,8 @@ $(document).ready(function () {
     let lat;
     let lon;
     let restaurantList = [];
-    const testURL = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lon}`;
+    let testURL;
+    // testURL = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lon}`;
     const baseURL = `https://developers.zomato.com/api/v2.1/search?`;
     const keyword = `q=${searchInput.val()}&`;
     const radius = `radius=${searchRadiusInput}&`;
@@ -17,17 +18,26 @@ $(document).ready(function () {
     const sortOrder = "order=" + "order box choice";
 
 
-    let muchies = {
+    let munchies = {
         getLocation: function () {
+            console.log(`getting location...`);
             navigator.geolocation.getCurrentPosition(function (response) {
+                
                 lat = response.coords.latitude;
                 lon = response.coords.longitude;
 
                 console.log(`lat:${lat}, lon:${lon}`);
+                //return {latitude:lat,longitude:lon};
+                $(`button`).click(function (event) {
+                    event.preventDefault();
+                    munchies.getData();
+                    //console.log(restaurantList);
+                })
             })
         },
 
         getData: function () {
+            testURL = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lon}`;
             $.ajax({
                 url: testURL,
                 method: "GET",
@@ -39,7 +49,14 @@ $(document).ready(function () {
                 console.log(response);
                 console.log(response.restaurants.length);
                 for (let i = 0; i < response.restaurants.length; i++) {
-                    restaurantList.push(response.restaurants[i].restaurant.name);
+                    let newRestaurant = {
+                        name: response.restaurants[i].restaurant.name,
+                        id: response.restaurants[i].restaurant.id,
+                        url: response.restaurants[i].restaurant.url,
+                        menu: response.restaurants[i].restaurant.menu_url,
+                        rating: response.restaurants[i].restaurant.user_rating.aggregate_rating
+                    }
+                    restaurantList.push(newRestaurant);
                 }
                 //restaurantList = response.restaurants.map(restaurant => { restaurant.restaurant.name });
                 console.log(restaurantList);
@@ -49,14 +66,14 @@ $(document).ready(function () {
 
     }
 
-
+    let coordinates = munchies.getLocation();
+    console.log(lat,lon);
+    // setTimeout(() => {
+    //     console.log(coordinates.latitude,coordinates.longitude);
+    // }, 2000); 
 
     //console.log(restaurantList);
-    $(`button`).click(function (event) {
-        event.preventDefault();
-        muchies.getData();
-        //console.log(restaurantList);
-    })
+    
 });
 
 
