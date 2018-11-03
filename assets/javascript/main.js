1faacc5fe17a5df7b37149b62391b4364323ee44
Mike Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-03 10:46:20 -0400

    updated js (#11)
    patr

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,30 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let lat;
     let lon;
+    let restaurantList;
 
-    navigator.geolocation.getCurrentPosition(function(response){
+    navigator.geolocation.getCurrentPosition(function (response) {
         lat = response.coords.latitude;
         lon = response.coords.longitude;
 
-        console.log(lat,lon);
+        console.log(`lat:${lat}, lon:${lon}`);
     })
+
+  const baseURL = "https://developers.zomato.com/api/v2.1/search?";
+  const keyword = "q=" + "searchbox value" + "&";
+  const radius = "radius=" + "value of search radius" + "&";
+  const location = "lat=" + lat + "&" + "lon=" + lon + "&";
+  const sort = "sort=" + "sort box choice" + "&";
+  const sortOrder = "order=" + "order box choice";  
+  
+  $.ajax({
+        url: "",
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
+        }
+    }).then(function (response) {
+        //console.log(response);
+        response.restaurants.map(restaurant=>{console.log(restaurant.restaurant.name)});
+
+    });
+
 })
 
-const baseURL = "https://developers.zomato.com/api/v2.1/search?";
-const keyword = "q=" + "searchbox value" + "&";
-const radius = "radius=" + "value of search radius" + "&";
-const location = "lat=" + lat + "&" + "lon=" + lon + "&";
-const sort = "sort=" + "sort box choice" + "&";
-const sortOrder = "order=" + "order box choice";
 
-$.ajax({
-    url: "https://developers.zomato.com/api/v2.1/search?entity_id=7555&entity_type=city",
-    method: "GET",
-    headers:{
-        "Accept": "application/json",
-        "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
-}
-  })
-    .then(function(response) {
-        console.log(response)
-    });
