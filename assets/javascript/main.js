7cdd77757dd8da2a333c3028630f994765a0c542
Patrick Gould <patrick.gould94@gmail.com>
2018-11-02 20:39:25 -0400

    added simple expression for converting miles to meters for radius search

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,5 @@
-$.ajax({
-    url: "https://developers.zomato.com/api/v2.1/search?entity_id=7555&entity_type=city",
-    method: "GET",
-    headers:{
-        "Accept": "application/json",
-        "user-key": "6baeb6d20512d445d4dd41fd5a72c19a"
-}
-  })
-    .then(function(response) {
-        console.log(response)
-    });
+
+
+let userSearchRadius = 0
+
+let convertedSearchRadius = userSearchRadius / 1609
